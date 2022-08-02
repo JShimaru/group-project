e1aa460235aed92d8b4f5ee6c9d69cbb0434ebd2
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-02 17:24:58 -0400

    Add header and routes/nav links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import {Routes, Route, NavLink} from 'react-router-dom';
 import Homepage from './Pages/Homepage';
+import ProgrammingQuotes from './Pages/ProgrammingQuotes';
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <header>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/programming'>Programming</NavLink>
+      </header>
+
+      <Routes>
+        <Route path='/' element={ <Homepage /> } />
+        <Route path='/programming' element={ <ProgrammingQuotes />} />
+        <Route path='*' />
+      </Routes>
     </div>
   );
 }
