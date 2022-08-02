67d042d57704d4fa733d1dc641b03a0f439a0dd7
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-02 17:38:05 -0400

    Create file that will handles API calls

---
+++ src/services/api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+async function randomGardenQuote() {
+  try {
+    const info = await axios.get('https://quote-garden.herokuapp.com/api/v3/quotes/random')
+    console.log(info)
+    return {quote: info.data.data[0].quoteText, author: info.data.data[0].quoteAuthor, genre: info.data.data[0].quoteGenre};
+  }
+  catch(e) {
+    console.error(e);
+    window.alert(e);
+  }
+}
+
+async function randomProgrammingQuote() {
+  try {
+    const info = await axios.get('https://programming-quotes-api.herokuapp.com/Quotes/random')
+    return {quote: info.data.en, author: info.data.author};
+  }
+  catch(e) {
+    console.error(e);
+    window.alert(e);
+  }
+}
+
+export {randomGardenQuote, randomProgrammingQuote};
