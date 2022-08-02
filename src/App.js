dd4b705dd38705451c31e869d21a80909221f801
Alisha Soriano <sorianoalisha@gmail.com>
2022-08-02 17:37:20 -0400

    Add route for famous quotes component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Routes, Route, NavLink} from 'react-router-dom';
 import Homepage from './Pages/Homepage';
 import ProgrammingQuotes from './Pages/ProgrammingQuotes';
+import FamousQuotes from './Pages/FamousQuotes';
 
 function App() {
   return (
@@ -9,11 +10,13 @@ function App() {
       <header>
         <NavLink to='/'>Home</NavLink>
         <NavLink to='/programming'>Programming</NavLink>
+        <NavLink to='/famous'>Famous</NavLink>
       </header>
 
       <Routes>
         <Route path='/' element={ <Homepage /> } />
         <Route path='/programming' element={ <ProgrammingQuotes />} />
+        <Route path='/famous' element={ <FamousQuotes />} />
         <Route path='*' />
       </Routes>
     </div>
